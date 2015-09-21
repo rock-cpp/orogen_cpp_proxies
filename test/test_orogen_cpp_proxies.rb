958c97963349036a5f4c4dd3eff2f685b8c82f96
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-09-21 14:00:25 +0200

    Initial commit

---
+++ test/test_orogen_cpp_proxies.rb
@@ -0,0 +1,24 @@
+require 'orogen_cpp_proxies/test'
+
+describe OrogenCppProxies do
+    include OrogenCppProxies::SelfTest
+
+    before do
+        # Code that is run before each test
+    end
+    after do
+        # Code that is run after each test
+    end
+
+    # We advise you to create one more "describe" subblock per method on the
+    # tested class. The before/after blocks from the parent scope are executed,
+    # and you can add some specific to this specific method
+    describe "#my_method" do
+        before do
+        end
+        after do
+        end
+        it "should do something right" do
+        end
+    end
+end
